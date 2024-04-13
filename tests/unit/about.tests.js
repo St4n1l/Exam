482cf20d0360ee2c7067ab78cd5bd67630c83513
St4n1l <stanil.h@abv.bg>
2024-04-13 14:22:13 +0300

    Fixed tests

--- tests/unit/about.tests.js
+++ tests/unit/about.tests.js
@@ -5,7 +5,7 @@ suite('About page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/about");
     let body = await res.text();
-    assert.ok(body.includes("<title>About</title>"));
     assert.ok(body.includes("<h1>About</h1>"));
+    assert.ok(body.includes("<p>This is the Regular exam for Software Engineering and DevOps course @ SoftUni</p>"));
   });
 });
